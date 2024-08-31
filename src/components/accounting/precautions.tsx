6dab4e2d24c945e5bc91191992ba5873c53650cb
masa <massanndesu52@gmail.com>
2024-08-31 21:15:53 +0900

    add:precautionsコンポーネントの文字を赤くした

--- src/components/accounting/precautions.tsx
+++ src/components/accounting/precautions.tsx
@@ -1,13 +1,15 @@
-import { Box } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import React from 'react'
 
 const Precautions = () => {
   return (
     <>
-    <Box>画面を閉じると注文番号が</Box>
-    <Box>わからなくなります。</Box>
-    <Box>お手数ですがこの画面を</Box>
-    <Box>閉じないでください</Box>
+      <Grid item xs={4} display={"flex"} flexDirection={"column"}>
+        <Box sx={{color: "red"}}>画面を閉じると注文番号が</Box>
+        <Box sx={{color: "red"}}>わからなくなります。</Box>
+        <Box sx={{color: "red"}}>お手数ですがこの画面を</Box>
+        <Box sx={{color: "red"}}>閉じないでください</Box>
+      </Grid>
     </>
   )
 }
