b9084f5a4bb8199f3b854252092035b8ca6721ab
masa <massanndesu52@gmail.com>
2024-09-03 16:10:23 +0900

    fix:文字の修正

--- src/components/accounting/precautions.tsx
+++ src/components/accounting/precautions.tsx
@@ -5,7 +5,7 @@ const Precautions = () => {
   return (
     <Grid alignItems="center" display="flex" flexDirection="column" item justifyContent="center" xs={4}>
       <Box sx={{ fontSize: '20px', color: 'red', textAlign: 'center' }}>画面を閉じると注文番号が</Box>
-      <Box sx={{ fontSize: '20px', color: 'red', textAlign: 'center' }}>わからなくなります。</Box>
+      <Box sx={{ fontSize: '20px', color: 'red', textAlign: 'center' }}>分からなくなります</Box>
       <Box sx={{ fontSize: '20px', color: 'red', textAlign: 'center' }}>お手数ですがこの画面を</Box>
       <Box sx={{ fontSize: '20px', color: 'red', textAlign: 'center' }}>閉じないでください</Box>
     </Grid>
