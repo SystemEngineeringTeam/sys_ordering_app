61e1bab0817d5a857192647fd4a69989af11fe4b
masa <massanndesu52@gmail.com>
2024-09-03 16:09:03 +0900

    fix:表示文字の修正

--- src/components/accounting/accounting_explanatory.tsx
+++ src/components/accounting/accounting_explanatory.tsx
@@ -7,7 +7,7 @@ const AccountingExplanatory = () => {
       <Box sx={{ height: '100px' }} />
       <Box sx={{ fontSize: '20px', textAlign: 'center' }}>予約ありがとうございます。</Box>
       <Box sx={{ height: '50px' }} />
-      <Box sx={{ fontSize: '20px', textAlign: 'center' }}>この画面をこの画面を店員に見せて,</Box>
+      <Box sx={{ fontSize: '20px', textAlign: 'center' }}>この画面を店員に見せて,</Box>
       <Box sx={{ fontSize: '20px', textAlign: 'center' }}>会計を済ませてください。</Box>
     </>
   );
