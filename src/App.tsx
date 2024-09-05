53016409870576544ec5f0079815fe5d4b46898b
masa <massanndesu52@gmail.com>
2024-09-05 15:35:17 +0900

    update:fmtとlintコマンドの実行

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css';
-import AccountingExplanatory from './components/accounting/accounting_explanatory';
 import Ordernumbar from './components/accounting/ordernumbar';
 import Precautions from './components/accounting/precautions';
 import WaitingPeople from './components/accounting/waiting_people';
@@ -7,8 +6,8 @@ import WaitingPeople from './components/accounting/waiting_people';
 const App = () => {
   return (
     <>
-      {/*<AccountingExplanatory */}
-      <WaitingPeople/>
+      {/* <AccountingExplanatory */}
+      <WaitingPeople />
       <Ordernumbar />
       <Precautions />
     </>
--- src/components/accounting/waiting_people.tsx
+++ src/components/accounting/waiting_people.tsx
@@ -1,13 +1,12 @@
-import { Box, Grid } from '@mui/material'
-import React from 'react'
+import { Box, Grid } from '@mui/material';
+import React from 'react';
 
 const WaitingPeople = () => {
   return (
     <Grid alignItems="center" display="flex" flexDirection="column" item justifyContent="center" xs={4}>
-        <Box sx={{marginBottom:'30px'}}>ご注文が確定しました</Box>
-        <Box sx={{fontSize:'30px'}}>待ち人数　4人</Box>
-     </Grid>
-    
+      <Box sx={{ marginBottom: '30px' }}>ご注文が確定しました</Box>
+      <Box sx={{ fontSize: '30px' }}>待ち人数 4人</Box>
+    </Grid>
   );
 };
 
