e2b55491be4297e7d8fbab0aee54098aa27e5868
masa <massanndesu52@gmail.com>
2024-09-05 15:28:18 +0900

    add: コンポーネントの追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 import AccountingExplanatory from './components/accounting/accounting_explanatory';
 import Ordernumbar from './components/accounting/ordernumbar';
 import Precautions from './components/accounting/precautions';
+import WaitingPeople from './components/accounting/waiting_people';
 
 const App = () => {
   return (
     <>
-      <AccountingExplanatory />
+      {/*<AccountingExplanatory */}
+      <WaitingPeople/>
       <Ordernumbar />
       <Precautions />
     </>
