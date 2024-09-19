000ad1205a42d20b0143ea97dcee3bcc32f187f4
masa <massanndesu52@gmail.com>
2024-09-19 13:34:58 +0900

    add:Toppingコンポーネントの内容追加

--- src/components/product-details/topping.tsx
+++ src/components/product-details/topping.tsx
@@ -1,7 +1,24 @@
+import { Box, Button } from '@mui/material';
 import React from 'react';
 
 const Topping = () => {
-  return <div>topping</div>;
+  return (
+    <>
+      <Box sx={{ fontSize: '1.2rem', color: 'gray' }}>トッピング</Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+        <Box sx={{ flexGrow: 1 }}>チーズ(+100円)</Box>
+        <Button variant="outlined" size="small" sx={{ color: 'gray', borderColor: 'gray' }}>なし</Button>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+        <Box sx={{ flexGrow: 1 }}>マヨネーズ</Box>
+        <Button variant="outlined" size="small" sx={{ color: 'gray', borderColor: 'gray' }}>なし</Button>
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', mb: 1 }}>
+        <Box sx={{ flexGrow: 1 }}>ケチャップ</Box>
+        <Button variant="outlined" size="small" sx={{ color: 'gray', borderColor: 'gray' }}>なし</Button>
+      </Box>
+    </>
+  )
 };
 
 export default Topping;
