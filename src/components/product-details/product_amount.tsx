ff93c474677d1cd56be22b18e2076d2e83d6b6fa
masa <massanndesu52@gmail.com>
2024-09-29 20:55:14 +0900

    add: ProductAmountコンポーネントの内容追加

--- src/components/product-details/product_amount.tsx
+++ src/components/product-details/product_amount.tsx
@@ -1,7 +1,52 @@
+import { Delete } from '@mui/icons-material';
+import { Box, Button, Stack } from '@mui/material';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 import React from 'react';
 
 const ProductAmount = () => {
-  return <div>product_amount</div>;
+  return (
+    <>
+      <Stack
+        alignItems="center"
+        direction="row"
+        justifyContent="flex-start"
+        spacing={1}
+        sx={{ width: '100%', paddingLeft: 2 }}
+      >
+        <Delete sx={{ color: 'gray', fontSize: '28px' }} />
+        <Box sx={{ fontSize: '20px' }}>1</Box>
+        <Button
+          sx={{
+            justifyContent: 'flex-start',
+            minWidth: 'auto',
+            padding: '6px',
+          }}
+        >
+          <AddCircleIcon sx={{ color: 'gray', fontSize: '28px' }} />
+        </Button>
+        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ fontSize: 25, paddingRight: 5 }}>9,999円</Box>
+      </Stack>
+      <Button
+        sx={{
+          backgroundColor: 'orange',
+          color: 'white',
+          '&:hover': {
+            backgroundColor: 'darkorange',
+          },
+          display: 'block',
+          margin: '0 auto',
+          marginTop: 2,
+          borderRadius: '20px',
+          width: '40%',
+          maxWidth: '250px',
+          padding: '6px 16px',
+        }}
+      >
+        <Box>カートに追加</Box>
+      </Button>
+    </>
+  );
 };
 
 export default ProductAmount;
