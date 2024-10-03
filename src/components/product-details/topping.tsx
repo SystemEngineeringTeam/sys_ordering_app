d250583d3bf6ca2e68cb314ceee692ef26637ab3
masa <massanndesu52@gmail.com>
2024-10-03 18:25:02 +0900

    update:lintとfmtの実行

--- src/components/product-details/topping.tsx
+++ src/components/product-details/topping.tsx
@@ -1,12 +1,12 @@
 import { Box, Button, Stack, Divider } from '@mui/material';
 import React from 'react';
 
-const ToppingName = ["チーズ", "マヨネーズ", "ケチャップ"];
+const ToppingName = ['チーズ', 'マヨネーズ', 'ケチャップ'];
 const ToppingPrice = [100, 0, 0];
 
 const Topping = () => {
   return (
-    <Stack spacing={2} sx={{ maxWidth: '90%',  marginLeft: '20px' }}>
+    <Stack spacing={2} sx={{ maxWidth: '90%', marginLeft: '20px' }}>
       <Box sx={{ fontSize: '1.2rem', color: 'gray' }}>トッピング</Box>
       <Stack divider={<Divider />} spacing={2}>
         {/* ToppingName配列の各要素に対してマッピングを行う */}
@@ -15,7 +15,8 @@ const Topping = () => {
           <Box key={name} sx={{ display: 'flex', alignItems: 'center' }}>
             {/* トッピング名と価格（価格が0より大きい場合のみ表示）を表示 */}
             <Box sx={{ flexGrow: 1 }}>
-              {name}{ToppingPrice[index] > 0 && `(+${ToppingPrice[index]}円)`}
+              {name}
+              {ToppingPrice[index] > 0 && `(+${ToppingPrice[index]}円)`}
             </Box>
             {/* 「なし」ボタンを表示 */}
             <Button
