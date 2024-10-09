bc615f293ed3ab5a19f3ad29dc676140369a71e1
masa <massanndesu52@gmail.com>
2024-10-09 17:02:26 +0900

    add: 時間を取得できるようにした(はず)

--- src/components/menuscrean/MenuItemsContainer.tsx
+++ src/components/menuscrean/MenuItemsContainer.tsx
@@ -6,6 +6,8 @@ import { category } from '@/types/type';
 import { useState } from 'react';
 import Grid2 from '@mui/material/Grid2';
 import ItemPopup from '@/pages/ItemPopup';
+import { useItem } from '@/firebase/useItem';
+import { menuItems } from '@/utils/menuItems';
 
 interface Props {
   activeCategory: category;
