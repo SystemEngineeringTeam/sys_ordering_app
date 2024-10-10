d2acabf9629d8301565edfd44e38221417b6c3de
higuchi-learn <higuchi.haruki.000@gmail.com>
2024-10-10 19:08:59 +0900

    修正addNewData

--- src/utils/addCart.ts
+++ src/utils/addCart.ts
@@ -1,22 +1,17 @@
 import { type options_id, type cartData } from '@/types/type';
 
-export function addNewData(NewCartData: cartData, Cart: cartData[], setCart: React.Dispatch<React.SetStateAction<cartData[]>>) {
-
+export function addNewData(
+  NewCartData: cartData,
+  Cart: cartData[],
+  setCart: React.Dispatch<React.SetStateAction<cartData[]>>,
+) {
   const NewCart = Cart;
-
-  console.dir('NewCartData78' + NewCartData, { depth: null })
-
   NewCart.push(NewCartData);
-
-  console.log('cartAfter' + Cart)
-
-  setCart(NewCart)
-
-  console.log('cart%%' + Cart)
+  setCart(NewCart);
 }
 
 export function someArrayChecker(arr1: options_id[], arr2: options_id[]) {
-  for (let i = 0; i < arr1.length; i + 1) {
+  for (let i = 0; i < arr1.length; i += 1) {
     if (arr1[i] !== arr2[i]) {
       return 0;
     }
@@ -24,8 +19,11 @@ export function someArrayChecker(arr1: options_id[], arr2: options_id[]) {
   return 1;
 }
 
-export function addCart(NewCartData: cartData, Cart: cartData[], setCart: React.Dispatch<React.SetStateAction<cartData[]>>) {
-  console.log("NewCartData333", NewCartData);
+export function addCart(
+  NewCartData: cartData,
+  Cart: cartData[],
+  setCart: React.Dispatch<React.SetStateAction<cartData[]>>,
+) {
   NewCartData.options.sort();
   if (Cart.length === undefined) {
     addNewData(NewCartData, Cart, setCart);
@@ -42,8 +40,6 @@ export function addCart(NewCartData: cartData, Cart: cartData[], setCart: React.
   if (SomeNameItemsFil === undefined) {
     addNewData(NewCartData, Cart, setCart);
     return;
-  } else {
-    console.log("elseNewCartData", NewCartData);
   }
   const SomeItemIndex = SomeNameItemsFil.findIndex(
     (cartData) => someArrayChecker(cartData.options, NewCartData.options) === 1,
@@ -51,17 +47,16 @@ export function addCart(NewCartData: cartData, Cart: cartData[], setCart: React.
   if (SomeItemIndex === -1) {
     addNewData(NewCartData, Cart, setCart);
     return;
-  } else {
-    const SomeCartDataIndex = Cart.findIndex(
-      (cartData) => cartData.timeStamp === SomeNameItemsFil[SomeItemIndex].timeStamp,
-    );
-    const UpdateCartData: cartData = {
-      itemId: Cart[SomeCartDataIndex].itemId,
-      options: Cart[SomeCartDataIndex].options,
-      qty: Cart[SomeCartDataIndex].qty + NewCartData.qty,
-      amountPrice: Cart[SomeCartDataIndex].amountPrice,
-      timeStamp: new Date().getTime(),
-    };
-    Cart[SomeCartDataIndex] = UpdateCartData;
   }
+  const SomeCartDataIndex = Cart.findIndex(
+    (cartData) => cartData.timeStamp === SomeNameItemsFil[SomeItemIndex].timeStamp,
+  );
+  const UpdateCartData: cartData = {
+    itemId: Cart[SomeCartDataIndex].itemId,
+    options: Cart[SomeCartDataIndex].options,
+    qty: Cart[SomeCartDataIndex].qty + NewCartData.qty,
+    amountPrice: Cart[SomeCartDataIndex].amountPrice + NewCartData.amountPrice,
+    timeStamp: new Date().getTime(),
+  };
+  Cart[SomeCartDataIndex] = UpdateCartData;
 }
